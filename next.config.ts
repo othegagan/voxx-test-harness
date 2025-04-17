04a1dc49edd5f5b617bc6ab14d6564e2f9f741e1
othegagan <hellothegagan@gmail.com>
2025-04-17 20:03:13 +0530

    iniy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.unsplash.com',
+                port: ''
+            },
+            {
+                protocol: 'https',
+                hostname: '**.pexels.com',
+                port: ''
+            }
+        ],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        formats: ['image/webp']
+    },
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '20mb'
+        }
+    }
 };
 
 export default nextConfig;
